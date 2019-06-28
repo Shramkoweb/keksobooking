8ec5f8d8584c5ad57d5f4bd3987f7b4e6740e23a
Serhii.Shramko <shramko.web@yahoo.com>
2019-06-28 17:06:15 +0300

    добавил методы для модуля util

--- js/util.js
+++ js/util.js
@@ -1,9 +1,22 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
   window.util = {
     getRandomInteger: function (min, max) { // получаем случайное число в заданом диапазоне включительно
       return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
     }
   };
 })();
