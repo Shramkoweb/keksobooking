cf659b4dc09e8ea60340c988df649d36363c615b
Serhii.Shramko <shramko.web@yahoo.com>
2019-06-28 21:02:51 +0300

    добавил обработчики для окна ошибки

--- js/show-error-popup.js
+++ js/show-error-popup.js
@@ -2,8 +2,28 @@
 
 (function () {
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var mainPage = document.querySelector('main');
+
+
+  var closeErrorPopup = function () {
+    mainPage.removeChild(errorTemplate);
+    document.removeEventListener('keydown', onErrorPopupEscPress);
+  };
+
+  var onErrorPopupEscPress = function (evt) {
+    window.util.isEscEvent(evt, closeErrorPopup);
+  };
 
   window.showErrorPopup = function () {
-    document.querySelector('main').appendChild(errorTemplate);
+    mainPage.appendChild(errorTemplate);
+    var errorButton = document.querySelector('.error__button');
+    var errorPopup = document.querySelector('.error');
+    document.addEventListener('keydown', onErrorPopupEscPress);
+    errorButton.addEventListener('click', function () {
+      closeErrorPopup();
+    });
+    errorPopup.addEventListener('click', function () {
+      closeErrorPopup();
+    });
   };
 })();
