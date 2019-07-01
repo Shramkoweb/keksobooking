2a838c6e34a041c79dfd47bafc9aec4141defb3a
Serhii.Shramko <shramko.web@yahoo.com>
2019-07-01 21:08:23 +0300

    ф-я дебоунса

---
+++ js/debounce.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var DEBOUNCE_INTERVAL = 500; // 0.5сек
+
+  var lastTimeout;
+  window.debounce = function (callback) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(callback, DEBOUNCE_INTERVAL);
+  };
+})();
