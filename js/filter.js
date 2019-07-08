6ae0a7a27b0399d553630985ad4c668f9252a801
Serhii.Shramko <shramko.web@yahoo.com>
2019-07-08 14:28:55 +0300

    новый модуль фильтрации (часть)

---
+++ js/filter.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+  var checkValue = function (elementValue, filterStateValue) {
+    return filterStateValue === 'any' || filterStateValue === elementValue.toString();
+  };
+
+  var checkGuest = function (elementValue, filterStateValue) {
+    return filterStateValue === 'any' || filterStateValue <= elementValue;
+  };
+
+  window.filter = function (ads) {
+    var filters = document.querySelector('.map__filters');
+    var filtersSelects = filters.querySelectorAll('select');
+    var filterState = {
+      features: []
+    };
+
+    filtersSelects.forEach(function (element) {
+      filterState[element.name.split('-')[1]] = element.value;
+    });
+
+    var filteredAds = ads.slice();
+    return filteredAds.filter(function (element) {
+      return checkValue(element.offer.type, filterState.type) && checkGuest(element.offer.guests, filterState.guests);
+    });
+  };
+})();
